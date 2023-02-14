b0eb78db4ee8d72408092d583c1e18bbdbc19752
vadimpechenin <65620788+vadimpechenin@users.noreply.github.com>
2023-02-14 16:32:23 +0400

    Update README.md

---
+++ ProcessingServer/src/main/java/MainServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello!");
+        printWriter.close();
+    }
+}
