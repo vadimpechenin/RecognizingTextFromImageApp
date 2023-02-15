0c5050a813cc5d2b6dbc9f5d6e3366ea51fd39fa
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2023-02-15 10:46:08 +0400

    MainServlet work now

--- ProcessingServer/src/main/java/MainServlet.java
+++ ProcessingServer/src/main/java/MainServlet.java
@@ -1,18 +1,45 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+
+/**
+ *
+ */
+@WebServlet("/handler")
 public class MainServlet extends HttpServlet {
     @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("Method init =)");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
+        super.service(req, resp);
+        resp.getWriter().write("Method service\n");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        log("Method desctoy =)");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
         printWriter.write("Hello!");
         printWriter.close();
+        resp.setStatus(200);
+        //resp.getWriter().write("Method doGet\n");
     }
 }
